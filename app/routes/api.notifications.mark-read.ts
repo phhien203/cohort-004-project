helderberto/cohort-003-project

06.04.02: Add in-app enrollment notifications for instructors
Co-Authored-By: Claude Opus 4.6 <noreply@anthropic.com>

app/routes/api.notifications.mark-read.ts
@@ -1,0 +1,36 @@
+import { data } from "react-router";
+import * as v from "valibot";
+import type { Route } from "./+types/api.notifications.mark-read";
+import { getCurrentUserId } from "~/lib/session";
+import { parseJsonBody } from "~/lib/validation";
+import { markAsRead, getNotifications } from "~/services/notificationService";
+
+const markReadSchema = v.object({
+  notificationId: v.number(),
+});
+
+export async function action({ request }: Route.ActionArgs) {
+  const currentUserId = await getCurrentUserId(request);
+  if (!currentUserId) {
+    throw data("Unauthorized", { status: 401 });
+  }
+
+  const parsed = await parseJsonBody(request, markReadSchema);
+
+  if (!parsed.success) {
+    throw data("Invalid parameters", { status: 400 });
+  }
+
+  const { notificationId } = parsed.data;
+
+  // Verify the notification belongs to the current user
+  const notifications = getNotifications(currentUserId, 1000, 0);
+  const owns = notifications.some((n) => n.id === notificationId);
+  if (!owns) {
+    throw data("Not found", { status: 404 });
+  }
+
+  markAsRead(notificationId);
+
+  return { success: true };
+}

phhien203/cohort-004-project

app/routes/api.notifications.mark-read.ts
@@ -1,0 +1,36 @@
+import { data } from "react-router";
+import { z } from "zod";
+import type { Route } from "./+types/api.notifications.mark-read";
+import { getCurrentUserId } from "~/lib/session";
+import { parseJsonBody } from "~/lib/validation";
+import { markAsRead, getNotifications } from "~/services/notificationService";
+
+const markReadSchema = z.object({
+  notificationId: z.number(),
+});
+
+export async function action({ request }: Route.ActionArgs) {
+  const currentUserId = await getCurrentUserId(request);
+  if (!currentUserId) {
+    throw data("Unauthorized", { status: 401 });
+  }
+
+  const parsed = await parseJsonBody(request, markReadSchema);
+
+  if (!parsed.success) {
+    throw data("Invalid parameters", { status: 400 });
+  }
+
+  const { notificationId } = parsed.data;
+
+  // Verify the notification belongs to the current user
+  const notifications = getNotifications(currentUserId, 1000, 0);
+  const owns = notifications.some((n) => n.id === notificationId);
+  if (!owns) {
+    throw data("Not found", { status: 404 });
+  }
+
+  markAsRead(notificationId);
+
+  return { success: true };
+}